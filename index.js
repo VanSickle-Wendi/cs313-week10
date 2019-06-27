e44a763f6494b1f32a2e54eaf85f1de9281e51d5
VanSickle-Wendi <gwendells@gmail.com>
2019-06-26 18:05:27 -0600

    Week 10 Team Assignment

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var app = express();
 
 const { Pool } = require('pg');
 
-const connectionString = process.env.DATABASE_URL || 'postgress://bvzlovwlmvybyc:428e6809e72e8ad2fee3eb1f4be3a3e6ba0c89903fa60e4b6ba8da6746bcdb3a@ec2-107-21-216-112.compute-1.amazonaws.com:5432/d93jk4ojlqvufu?ssl=true'
+const connectionString = process.env.DATABASE_URL || 'postgress://bvzlovwlmvybyc:428e6809e72e8ad2fee3eb1f4be3a3e6ba0c89903fa60e4b6ba8da6746bcdb3a@ec2-107-21-216-112.compute-1.amazonaws.com:5432/d93jk4ojlqvufu?ssl=true';
 
 const pool = new Pool({connectionString: connectionString});
 
@@ -26,18 +26,21 @@ function getPerson(req, res) {
    console.log("Retrieving person with id: ", id);
    
    getPersonFromDb(id, function(error, result) {
-      console.log("Back from the getPersonFromBb function: ", result);
+     console.log("Back from the getPersonFromBb function: ", result); 
+     
+      if (error || result == null || result.length != 1) {
+         res.status(500).json({success:false, data: error});
+      } else {
+         res.json(result[0]);
+      }      
+
    });
-   
-   var result = {id: 2, first_name: "Wendi", last_name: "Van Sickle", dob: "1969-06-28"};
-   
-   res.json(result);
 }
 
 function getPersonFromDb(id, callback) {
    console.log("getPersonFromDb called with id ", id);
    
-   var sql = "SELECT id, first, last, birthdate From person WHERE ID = $1::int";
+   var sql = "SELECT id, first_name, last_name, dob From person WHERE ID = $1::int";
    var params = [id];
    
    pool.query(sql, params, function(err, result) {
